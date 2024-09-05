c3d34051ad1afda74ad2a40c0247954858638866
Francis Lagares <francis.lagares@gmail.com>
2024-09-05 11:27:06 +0200

    feat: create ReactQueryProvider setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom/client';
 
 import App from '@/App';
+
+import Providers from './providers';
 
 import '@/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+      <ReactQueryDevtools />
+    </Providers>
   </React.StrictMode>,
 );
---
+++ src/providers/index.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from 'react';
+
+import ReactQueryProvider from './ReactQueryProvider';
+
+const Providers = ({ children }: PropsWithChildren) => {
+  return <ReactQueryProvider>{children}</ReactQueryProvider>;
+};
+
+export default Providers;
